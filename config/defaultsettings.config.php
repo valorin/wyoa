d2685e5806efe463b3470ac2ffeef0cabbaaebac
Stephen Rees-Carter <stephen@rees-carter.net>
2012-05-06 17:18:49 +1000

    Implemented basic settings class.

---
+++ config/defaultsettings.config.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Default Setting Values
+ *
+ */
+return array(
+    'title'   => "Write Your Own Adventure",
+    'tagline' => "The collaborative 'choose your own adventure' generator.",
+);
