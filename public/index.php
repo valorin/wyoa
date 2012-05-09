2f502d1ba4d3aa8f91a20de38d50e23a64952c5d
Stephen Rees-Carter <stephen@rees-carter.net>
2012-05-09 20:10:09 +1000

    Updated with new version from the ZF skeleton app.

--- public/index.php
+++ public/index.php
@@ -9,17 +9,17 @@ $sharedEvents     = new Zend\EventManager\SharedEventManager();
 $listenerOptions  = new Zend\Module\Listener\ListenerOptions($appConfig['module_listener_options']);
 $defaultListeners = new Zend\Module\Listener\DefaultListenerAggregate($listenerOptions);
 $defaultListeners->getConfigListener()->addConfigGlobPath("config/autoload/*.php");
-    
+
 
 $moduleManager = new Zend\Module\Manager($appConfig['modules']);
 $events        = $moduleManager->events();
-$events->setSharedCollections($sharedEvents);
+$events->setSharedManager($sharedEvents);
 $events->attach($defaultListeners);
 $moduleManager->loadModules();
 
 // Create application, bootstrap, and run
 $bootstrap   = new Zend\Mvc\Bootstrap($defaultListeners->getConfigListener()->getMergedConfig());
-$bootstrap->events()->setSharedCollections($sharedEvents);
+$bootstrap->events()->setSharedManager($sharedEvents);
 $application = new Zend\Mvc\Application;
 $bootstrap->bootstrap($application);
 $application->run()->send();
