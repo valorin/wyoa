616b5af24877c4279024130361eef2fcbe63598a
Stephen Rees-Carter <stephen@rees-carter.net>
2012-05-10 17:36:43 +1000

    Adding in jenkins test framework.

---
+++ tests/Application/DummyTest.php
@@ -0,0 +1,9 @@
+<?php
+
+class Application_DummyTest extends PHPUnit_Framework_TestCase
+{
+    public function testDummy()
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/Story/DummyTest.php
@@ -0,0 +1,9 @@
+<?php
+
+class Story_DummyTest extends PHPUnit_Framework_TestCase
+{
+    public function testDummy()
+    {
+        $this->assertTrue(true);
+    }
+}
