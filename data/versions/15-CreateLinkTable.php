fe156ddc74754c934a7372e3cc0e8c19013f6ef9
Stephen Rees-Carter <stephen@rees-carter.net>
2012-05-10 21:26:32 +1000

    Started implementing the version scripts.

---
+++ data/versions/15-CreateLinkTable.php
@@ -0,0 +1,19 @@
+<?php
+namespace Application\Version;
+use Zend\Db\Adapter\Adapter,
+    Version\Model\AbstractVersion;
+
+/**
+ * Version Script - Create Link Table
+ *
+ * Creates the `link` table in the db.
+ *
+ * @package     WYOA
+ * @subpackage  Application\Version
+ * @copyright   Copyright (c) 2012, Stephen Rees-Carter <http://src.id.au/>
+ * @license     New BSD Licence, see LICENCE.txt
+ */
+class CreateLinkTable extends AbstractVersion
+{
+
+}
---
+++ data/versions/5-CreateUserTable.php
@@ -0,0 +1,19 @@
+<?php
+namespace Application\Version;
+use Zend\Db\Adapter\Adapter,
+    Version\Model\AbstractVersion;
+
+/**
+ * Version Script - Create User Table
+ *
+ * Creates the `user` table in the db.
+ *
+ * @package     WYOA
+ * @subpackage  Application\Version
+ * @copyright   Copyright (c) 2012, Stephen Rees-Carter <http://src.id.au/>
+ * @license     New BSD Licence, see LICENCE.txt
+ */
+class CreateUserTable extends AbstractVersion
+{
+
+}
